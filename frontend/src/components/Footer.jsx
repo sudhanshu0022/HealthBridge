12de00e64a1bbf4251bb116e71e99d49dd4c9a37
sudhanshu0022 <agarwalsudhanshu772@gmail.com>
2026-05-26 22:21:11 +0530

    edit about

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
            <div>
             <img className='mb-5 w-40' src={assets.logo} alt="logo" />
-            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Your Company Name</p>
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>HealthBridge is a leading healthcare platform dedicated to providing accessible and quality medical services to patients around the globe.</p>
            </div>
 
            <div>
