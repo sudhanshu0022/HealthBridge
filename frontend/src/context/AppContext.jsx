4e64cc7e7887b5eaa377d45fdcee136acf105b89
sudhanshu0022 <agarwalsudhanshu772@gmail.com>
2026-05-27 14:37:54 +0530

    rupees symbol

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const currencySymbol = '$';
+    const currencySymbol = '₹';
     const backendUrl = "https://backend-pqmw.onrender.com"; // Local backend development server port
 
     const [doctors, setDoctors] = useState([]);
